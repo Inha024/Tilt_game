6f29ffe5c8b5431bb6d9cf49656c173d8fb09920
Ingrid <ingridmh99@hotmail.com>
2020-06-01 22:31:31 +0200

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+     if (other.name == "Goal")
+        {
+            Destroy(gameObject, .4f);
+            Debug.Log("You Win!");
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Tilt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    public Vector3 currentRot;
+
+    void Update()
+    {
+        currentRot = GetComponent<Transform>().eulerAngles;
+
+        if ((Input.GetAxis("Horizontal") > .2) && (currentRot.z <=10 || currentRot.z >=348))
+        {
+            transform.Rotate(0, 0, 1);
+        }
+
+        if ((Input.GetAxis("Horizontal") < -.2) && (currentRot.z >=349 || currentRot.z <=11))
+        {
+            transform.Rotate(0, 0, -1);
+        }
+
+
+        if ((Input.GetAxis("Vertical") > .2) && (currentRot.x <= 10 || currentRot.x >= 348))
+        {
+            transform.Rotate(1, 0, 0);
+        }
+
+        if ((Input.GetAxis("Vertical") < -.2) && (currentRot.x >= 349 || currentRot.x <= 11))
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+
+
+    }
+}
